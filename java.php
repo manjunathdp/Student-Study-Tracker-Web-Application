d5ec7dedd8837ad6ef24e3ba95744831cb7c2921
manjunathdp <manjunathdtc3@gmail.com>
2022-08-07 01:44:37 +0530

    List of hours studied in every month added

--- java.php
+++ java.php
@@ -43,6 +43,27 @@ $dbrt=mysqli_query($conn,$ret2);
     ?>
         </table>
         <h1>Total hours=<?php echo $totalHrs ?></h1>
+        <center><h1 style="background-color:powderblue;"><u><i>List of hours studied in every month</i></u></h1></center>
+        <table border=1px solid black class=toptables><tr>
+        <th>MONTH(YEAR)</th>
+        <th>HOURS</th></tr>
+        <?php
+        $ret2="select DATE_FORMAT(date ,'%M (%Y)') as month,sum(hours) as hrs from details WHERE subject='JAVA' group by month;";
+
+        $dbrt=mysqli_query($conn,$ret2);
+    
+         while ($arr = mysqli_fetch_assoc($dbrt))
+    {
+       
+?>
+        <tr>
+            <td><?php echo $arr['month']; ?></td>
+            <td><?php echo $arr['hrs']; ?></td>
+            
+        </tr>
+<?php
+    }
+    ?>
         </body>
     </html>
     
