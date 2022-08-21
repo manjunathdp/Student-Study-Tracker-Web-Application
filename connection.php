90a74e70bb024bc6285df98240ab46f65aa7c875
manjunathdp <manjunathdtc3@gmail.com>
2022-08-21 17:35:18 +0530

    date added

--- connection.php
+++ connection.php
@@ -5,6 +5,8 @@
 	 $dbName="mydetails";
 	 $totalHrs=0;
 	 $conn=mysqli_connect($serverName,$userName,$password,$dbName);
+	 date_default_timezone_set("Asia/Kolkata");   //India time (GMT+5:30)
+	 $Date=date('Y-m-d');
 	 	
 	 ?>
 
