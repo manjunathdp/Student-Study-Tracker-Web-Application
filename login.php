b3dca8927649fda51679ba23e9d28a11cd464d9e
Manjunath Pujar <101979415+manjunathdp@users.noreply.github.com>
2024-09-17 13:34:19 +0530

    code updated
    there was problem after hosting so because of that code has been updated

--- login.php
+++ login.php
@@ -1,25 +1,36 @@
 <?php
 include("connection.php");
-if(!empty($_POST['login']))
-{
-    echo "jyvwduv";
-    $uname=$_POST['username'];
-    $pass=$_POST['pass'];
-    $query="select * from login where username='$uname' and password='$pass'";
-    $result=mysqli_query($conn,$query);
-    $count=mysqli_num_rows($result);
-    if($count>0)
-    {
-        header("Location: home.html");
-        exit();
-    }
-    else
-    {
-        echo "<script>
-		alert('Enter valid Username / Password');
-		window.location.href='index.html';
-		</script>";
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+if (!empty($_POST['login'])) {
+    // echo "1";
+    $uname = $_POST['username'];
+    $pass = $_POST['pass'];
+
+    // echo "2";
+    $query = "SELECT * FROM signup WHERE username='$uname' AND password='$pass'";
+    echo "Query: " . $query;
+
+    $result = mysqli_query($conn, $query);
+
+    if (!$result) {
+        echo "MySQL Error: " . mysqli_error($conn);
     }
 
+    // echo "4";
+    $count = mysqli_num_rows($result);
+
+    if ($count > 0) {
+        // echo "if 1";
+        header("Location: home.html");
+        exit();
+    } else {
+        // echo "else 1";
+        echo "<script>
+            alert('Enter valid Username / Password');
+            window.location.href='index.html';
+            </script>";
+    }
 }
 ?>
